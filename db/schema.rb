f0b840d50046041dbd6aab3b40a0328f6b6d1b75
shoji-ippei <i.shoji.cubic@gmail.com>
2016-07-22 11:47:10 +0900

    ポストのユーザーページへの表示

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 20160720073532) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
