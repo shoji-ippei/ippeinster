9a3d397fd4ea6314cab5619daeb67819b374dce1
shoji-ippei <i.shoji.cubic@gmail.com>
2016-07-29 12:22:41 +0900

    エラー修正login_test

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -33,9 +33,9 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     assert_not is_logged_in?
     assert_redirected_to root_url
     follow_redirect!
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 
   test "login with remembering" do
